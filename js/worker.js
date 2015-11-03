77137bf8051272d3bf3f40f9f069d4ae14be8543
gaoshi-github <13701378834@163.com>
2015-11-03 17:19:45 +0800

    部署
    sui

--- js/worker.js
+++
@@ -1,9 +0,0 @@
-//worker.js
-onmessage = function(evt) {
-	var data = {
-		title : "HTML5桌面提醒",
-		msg : "提示信息",
-		ic : "https://www.baidu.com/img/bd_logo1.png"
-	}
-	postMessage(data); //将获取到的数据发送会主线程
-}
