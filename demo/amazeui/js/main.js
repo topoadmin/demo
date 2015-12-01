c040956694cdb870e8d86b91c255e084404706ca
gaoshi-github <13701378834@163.com>
2015-12-01 18:06:54 +0800

    改

--- demo/amazeui/js/main.js
+++ demo/amazeui/js/main.js
@@ -1,12 +1,13 @@
 /*入口脚本*/
 require.config({
     paths: {	// -- 配置别名
-    	"jquery":"../assets/js/jquery.min",
     	"amazeui":"../assets/js/amazeui.min",
     	"app":"module/app",
     	"user":"user",
     	"countUp":"module/countUp",
-    	"autoexec":"module/autoexec"
+    	"autoexec":"module/autoexec",
+    	"datetime":"module/amazeui.datetimepicker",
+    	"chosen":"module/amazeui.chosen"
     }
 });
 require(["amazeui"],function(){
