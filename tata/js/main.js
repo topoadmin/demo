240c1cbd9de7b3bbe51cf063eb5b326b1234e847
gaoshi-github <13701378834@163.com>
2015-12-14 18:07:54 +0800

    添加活动

--- tata/js/main.js
+++ tata/js/main.js
@@ -5,6 +5,7 @@ require.config({
     	"jquery":"../assets/js/jquery.min",
 //		"jquery":"http://libs.baidu.com/jquery/2.0.0/jquery.min",	百度cdn搭配妹子会出BUG
     	"amazeui":"http://cdn.amazeui.org/amazeui/2.5.0/js/amazeui.min",
+    	"laytpl":"module/laytpl",
     	"app":"page/app",
     	"home":"page/home"
     }
--- tata/js/page/home.js
+++ tata/js/page/home.js
@@ -1,23 +1,41 @@
 (function(root, factory) {
 	"use strict";
 	if (typeof define === "function" && define.amd) {
-		define(["jquery"], factory);
+		define(["jquery","laytpl"], factory);
 	} else if (typeof exports === "object") {
-		module.exports = factory(require("jquery"));
+		module.exports = factory(require("jquery"),require("laytpl"));
 	} else {
 		factory(root.jQuery);
 	}
 
-}(this, function($) {
-	$('#home-carousel').flexslider({
-		playAfterPaused: 8000,
-		slideshowSpeed: 50000,
-		controlNav: false
+}(this, function($,laytpl) {
+	$.getJSON("js/data/activity-soft.json",function(data){
+		addActivity("activity-tpl","activity-soft",data);
+		addGallery(document.getElementById("activity-soft"));
 	});
 	
-	$(".fixed-widtn").on("click",function(){
-		var fixedTxt = $(this).children(".fixed-txt"),txt = fixedTxt.text().trim();
-		fixedTxt.text((txt == "宽屏") ? "窄屏":"宽屏")
+	$.getJSON("js/data/activity-soft.json",function(data){
+		addActivity("activity-tpl","activity-box",data);
+		addGallery(document.getElementById("activity-box"));
+	});
+	
+	function addActivity(tplId,setDomId,data){
+		var gettpl = document.getElementById(tplId).innerHTML;
+		laytpl(gettpl).render(data, function(html) {
+			document.getElementById(setDomId).innerHTML = html;
+		});
+	}
+	
+	$('#home-carousel').flexslider({
+		playAfterPaused: 8000,
+		slideshowSpeed: 3000,
+		controlNav: false
+	});
+
+	$(".fixed-widtn").on("click", function() {
+		var fixedTxt = $(this).children(".fixed-txt"),
+			txt = fixedTxt.text().trim();
+		fixedTxt.text((txt == "宽屏") ? "窄屏" : "宽屏")
 		$("body").toggleClass("am-g-fixed-1200");
 		$(window).trigger("resize"); // 触发resize事件,轮播重设宽度
 	})
