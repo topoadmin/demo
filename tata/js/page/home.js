561538a9b20ff8605789897ed72c5187db3cb383
gaoshi-github <13701378834@163.com>
2015-12-21 11:11:04 +0800

    测试hbuider提交

--- tata/js/page/home.js
+++ tata/js/page/home.js
@@ -8,7 +8,53 @@
 		factory(root.jQuery);
 	}
 }(this, function($, laytpl) {
-	// 促销活动
+	// -- 开启轮播
+	$("#home-carousel").flexslider({
+		slideshowSpeed: 3000,
+		controlNav: false
+	})
+	
+	// -- 添加用户
+	$.getJSON("js/data/user.json", function(data) {
+		var gettpl = '<div class="am-g"><div class="am-slider am-slider-default am-slider-carousel am-u-sm-10 am-u-sm-push-1"><ul class="am-slides">{{# for(var i = 0, len = d.length; i< len; i++){ }} <li><div class="am-gallery-item am-text-center" style="position: relative;"><a class="user-img-box" href="#"><img class="user-avatar am-center lazyload" data-original="{{ d[i].avatar }}" src="img/load.gif" alt="{{ d[i].name }}" /><img class="user-rank am-show-lg-up" src="img/vip-{{ d[i].rank }}.png" /></a><span class="am-text-sm am-text-secondary">{{ d[i].name }}</span></div></li>{{# } }}</ul></div></div>'
+		laytpl(gettpl).render(data, function(html) {
+			document.getElementById("user-box").innerHTML = html;
+		});
+		var $userBox = $("#user-box");
+		// 开启赖加载 绑定sporty事件立即执行
+		var userLazyLoad = $userBox.find(".lazyload");
+		userLazyLoad.lazyload({
+			event: "sporty"
+		});
+		var iw; // 获取用户图片指定宽度
+		if ($userBox.width() > 1000) {
+			iw = $userBox.width() / 6;
+			// 打开页面时加载前7个用户头像
+			userLazyLoad.each(function(index) {
+				if (index < 7) {
+					$(this).trigger("sporty")
+				}
+			})
+		} else {
+			iw = $userBox.width() / 3;
+			// 打开页面时加载前4个用户头像
+			userLazyLoad.each(function(index) {
+				if (index < 4) {
+					$(this).trigger("sporty")
+				}
+			})
+		}
+		$userBox.find(".am-slider").flexslider({
+			itemWidth: iw,
+			itemMargin: 5,
+			pauseOnHover: true,
+			slideshowSpeed: 300000,
+			after: function() {
+				userLazyLoad.trigger("sporty");
+			}
+		});
+	});
+	// -- 促销活动
 	$.getJSON("js/data/activity-soft.json", function(data) {
 		addActivity("activity-tpl", "activity-soft", data);
 		//addGallery(document.getElementById("activity-soft"));
@@ -16,7 +62,7 @@
 			threshold: 280
 		});
 	});
-	// 所有活动
+	// -- 所有活动
 	$.getJSON("js/data/activity-box.json", function(data) {
 		addActivity("activity-tpl", "activity-box", data);
 		$("#activity-box .lazyload").lazyload({
@@ -24,57 +70,8 @@
 		});
 	});
 	
-	// 添加用户
-	$.getJSON("js/data/user.json", function(data) {
-		var gettpl = document.getElementById("user-tpl").innerHTML;
-		laytpl(gettpl).render(data, function(html) {
-			document.getElementById("user-box").innerHTML = html;
-		});
-		var $userBox = $("#user-box");
-		
-		// 开启赖加载 绑定sporty事件立即执行
-		var userLazyLoad = $userBox.find(".lazyload");
-		userLazyLoad.lazyload({
-			event : "sporty"  
-		});
-		var iw;	// 获取用户图片指定宽度
-		if($userBox.width() > 1000){
-			iw = $userBox.width()/6;
-			// 打开页面时加载前7个用户头像
-			userLazyLoad.each(function(index){
-				if(index < 7){
-					$(this).trigger("sporty")
-				}
-			})
-		}else{
-			iw = $userBox.width()/3;
-			// 打开页面时加载前4个用户头像
-			userLazyLoad.each(function(index){
-				if(index < 4){
-					$(this).trigger("sporty")
-				}
-			})
-		}
-		
-		
-		$userBox.find(".am-slider").flexslider({
-			itemWidth: iw,
-			itemMargin: 5,
-			pauseOnHover: true,
-			slideshowSpeed: 300000,
-			after:function(){
-				userLazyLoad.trigger("sporty");
-			}
-		});
-	});
-	
-	$("#home-carousel").flexslider({
-		slideshowSpeed : 3000,
-		controlNav: false
-	})
-
+	// -- mixitup 排序分类
 	require(["mixitup"], function() {
-		// -- mixitup 排序分类
 		var filter = $("#classify .filter"); // -- 分类按钮节点
 		filter.on("click", function() {
 			$(this).addClass("am-active").siblings(".filter").removeClass("am-active");
@@ -94,13 +91,15 @@
 		});
 	});
 
-	// 添加模版
+	// -- 活动模版
 	function addActivity(tplId, setDomId, data) {
 		var gettpl = '<ul class="am-gallery am-avg-sm-2 am-avg-md-3 am-avg-lg-5 am-gallery-imgbordered">{{# for(var i = 0, len = d.length; i< len; i++){ }} {{# if(d[i].privilege){ }} <li class="all {{ d[i].site }} 特价">{{# }else{ }}<li class="all {{ d[i].site }}">{{# }}}<div class="am-gallery-item"><a href="{{ d[i].href }}"><img class="lazyload" data-original="{{ d[i].img }}" src="img/load.gif" alt="{{ d[i].alt }}" /><h3 class="am-gallery-title">{{ d[i].site }} - {{ d[i].site2 }}</h3><div class="am-gallery-desc"><span>出发时间:</span><span>{{ d[i].time }}</span><div class="am-text-lg">{{# if(d[i].privilege){ }}<del>￥:<span class="am-text-sm">{{ d[i].price }}.00</span></del> <span class="am-text-danger am-text-sm">{{ d[i].privilege }}.00</span> {{# }else{ }} ￥:<span class="am-text-danger">{{ d[i].price }}.00</span> {{# }}}</div></div></a></div></li>{{# } }}</ul>'
 		laytpl(gettpl).render(data, function(html) {
 			document.getElementById(setDomId).innerHTML = html;
 		});
 	}
+	
+	
 	// 给am-gallery-item添加动画
 	// -- 移动端效果不佳 暂不开启
 	function addGallery(elm) {
