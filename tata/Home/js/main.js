63b6d5b890388326450be80259df0b63735cec1a
gaoshi-github <13701378834@163.com>
2016-01-05 17:54:44 +0800

    修改文件结构，适应PHP习惯

--- tata/Home/js/main.js
+++ tata/Home/js/main.js
@@ -1,7 +1,7 @@
 require.config({
 	paths: {
-		  	"amazeui":"../assets/js/amazeui.min",	// 妹子Ui
-		"jquery": "../assets/js/jquery.min",
+		 "amazeui":"../../assets/js/amazeui.min",	// 妹子Ui
+		"jquery": "../../assets/js/jquery.min",
 		//		"jquery":"http://libs.baidu.com/jquery/2.0.0/jquery.min",	百度cdn搭配妹子会出BUG
 //		"amazeui": "http://cdn.amazeui.org/amazeui/2.5.0/js/amazeui.min",
 		"lazyload": "module/jquery.lazyload",
--- tata/Home/js/page/article.js
+++ tata/Home/js/page/article.js
@@ -15,20 +15,20 @@
 
 	/*    -- 侦测导航 --    */
 	var at = $("#article-template"), // 需要侦测的盒子
-		atNav = at.find(".at-nav"),	// 需要侦测的导航
+		atNav = at.find(".at-nav"), // 需要侦测的导航
 		atRight = at.find(".c-right");
-	if(!atNav.is(":hidden")){
+	if (!atNav.is(":hidden")) {
 		atNav.sticky({
 			top: 61
 		}).scrollspynav();
 	}
-	
-	$(window).on("scroll",function(){
+
+	$(window).on("scroll", function() {
 		var leftTop = atNav.offset().top + atNav.height();
 		var rightTop = atRight.offset().top + atRight.height();
-		if(leftTop >= rightTop-20){
+		if (leftTop >= rightTop - 20) {
 			atNav.hide();
-		}else{
+		} else {
 			atNav.show();
 		}
 	});
