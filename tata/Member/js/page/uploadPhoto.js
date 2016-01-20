52f70653221a02d613f9ea6459ad13d877626252
gaoshi-github <13701378834@163.com>
2016-01-20 14:21:00 +0800

    保存文件

---
+++ tata/Member/js/page/uploadPhoto.js
@@ -0,0 +1,12 @@
+(function(root, factory) {
+	"use strict";
+	if (typeof define === "function" && define.amd) {
+		define(["jquery"], factory);
+	} else if (typeof exports === "object") {
+		module.exports = factory(require("jquery"), require("amazeui"));
+	} else {
+		factory(root.jQuery);
+	}
+
+}(this, function($) {
+}));
