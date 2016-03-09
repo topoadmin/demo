52c682339109bd53acbc02f27ff9cb3fc971af6d
gaoshi-github <13701378834@163.com>
2016-03-09 17:15:19 +0800

    添加 ng demo

---
+++ common/js/app.js
@@ -0,0 +1,42 @@
+define(["angular", "angularUiRouter"], function(angular) {
+	var app = angular.module("admin", ["ui.router", "nav-module"]);
+	app.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise("/index");
+		$stateProvider
+			.state('index', {
+				url: '/index',
+				views: {
+					'': {
+						templateUrl: 'tpls/index.html'
+					},
+					'nav@index': {
+						templateUrl: 'tpls/nav.html',
+						controller: "my-nav"
+					},
+					'main@index': {
+						templateUrl: 'tpls/commonFn.html'
+					}
+				}
+			})
+			.state('index.demo', {
+				url: '/demo',
+				views: {
+					'main@index': {
+						templateUrl: 'tpls/demo.html'
+					}
+				}
+			})
+	});
+
+	// --生成导航
+	var navModule = angular.module("nav-module", []);
+	navModule.controller('my-nav', function($scope) {
+		$scope.items = [{
+			title: "常用函数",
+			url: "index"
+		}, {
+			title: "测试多层次路由",
+			url: "index.demo"
+		}];
+	});
+});
---
+++ common/js/main.js
@@ -0,0 +1,21 @@
+require.config({
+	baseUrl:"../",
+	paths: {
+		"angular": "build/js/angular.min",
+		"angularUiRouter":"build/js/angular-ui-router.min",
+		"app" : "common/js/app"
+	},
+	shim: {
+		"angular": {
+			exports: "angular"
+		},
+		'angularUiRouter':{
+          deps: ["angular"],
+          exports: 'angularUiRouter'
+       },
+		
+	}
+});
+require(["angular","angularUiRouter","app"], function(angular) {
+	angular.bootstrap(document, ['admin']);
+});
